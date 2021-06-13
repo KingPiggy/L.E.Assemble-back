a2fae7c8e4605c70873173c69e85e640b0b7ed68
HoonDragonite <lsoonnoon@gmail.com>
2021-06-13 23:18:31 +0900

    Feat: Create pid file

--- src/main/java/com/hoondragonite/leassamble/Application.java
+++ src/main/java/com/hoondragonite/leassamble/Application.java
@@ -2,12 +2,15 @@ package com.hoondragonite.leassamble;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(new ApplicationPidFileWriter());   // pid 를 작성하는 역할을 하는 클래스 선언
+        app.run(args);
     }
 
 }
