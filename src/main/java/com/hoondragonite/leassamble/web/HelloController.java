b0721c3e3f0c53ca555552139901ced3e3133113
HoonDragonite <lsoonnoon@gmail.com>
2021-06-15 16:17:20 +0900

    Build: test deploy

--- src/main/java/com/hoondragonite/leassamble/web/HelloController.java
+++ src/main/java/com/hoondragonite/leassamble/web/HelloController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    
+    // 테스트용
     @GetMapping("/hello")
     public String hello() {
         return "hello";
