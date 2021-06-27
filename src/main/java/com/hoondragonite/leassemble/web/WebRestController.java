0899d022b1ccf7494dec3e0d9dedd9f50211a8b0
HoonDragonite <lsoonnoon@gmail.com>
2021-06-27 17:21:38 +0900

    Feat: Add port-test API

--- src/main/java/com/hoondragonite/leassemble/web/WebRestController.java
+++ src/main/java/com/hoondragonite/leassemble/web/WebRestController.java
@@ -2,18 +2,23 @@ package com.hoondragonite.leassemble.web;
 
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
-
-    @PostMapping("/api/port-test1")
-    public ResponseEntity<String> portTest1(HttpServletRequest request) {
-        return ResponseEntity.ok("8081 테스트");
+    @GetMapping("/api/port-test")
+    public ResponseEntity<String> portTest(HttpServletRequest request) {
+        return ResponseEntity.ok("8080 리턴값 받아라!");
     }
-    @PostMapping("/api/port-test2")
+
+    @GetMapping("/api/port-test1")
+    public ResponseEntity<String> portTest1(HttpServletRequest request) {
+        return ResponseEntity.ok("8081 리턴값 받아라!");
+    }
+    @GetMapping("/api/port-test2")
     public ResponseEntity<String> portTest2(HttpServletRequest request) {
-        return ResponseEntity.ok("8082 테스트");
+        return ResponseEntity.ok("8082 리턴값 받아라!");
     }
 }
