f3c2ae5a5e7068b2e9200a0eb49cbea1460f209e
HoonDragonite <lsoonnoon@gmail.com>
2021-08-16 15:58:04 +0900

    Fix: Edit Static file path

--- src/main/java/com/hoondragonite/leassemble/web/ProfileController.java
+++ src/main/java/com/hoondragonite/leassemble/web/ProfileController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
