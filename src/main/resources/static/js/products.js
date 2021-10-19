b3207becb057b8a48ee44ebb42656aac07b9ce17
HoonDragonite <lsoonnoon@gmail.com>
2021-10-19 16:04:40 +0900

    Feat: 상품 수정기능 구현

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -2,43 +2,78 @@ function doCreateProduct(){
     let sendData = {
             name : ($('#productNameForm').val()),
             info : $('#productInfoForm').val(),
-            tel : $('#productPriceForm').val()
+            price : $('#productPriceForm').val().replace(/\,/g,'')
     };
 
     var storeId = $('#storeId').text();
 
     console.log(sendData);
-    console.log(storeId);
+
+    $.ajax({
+                type: "POST",
+                url: "/api/user/stores/" + storeId +
+                    "/products",
+                dataType: "JSON",
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(sendData),
+                success: function(data) {
+                    console.log("저장 성공! uID is " + data);
+                    location.href="/my-store/" + storeId + "/products";
+                },
+                error:function(request, status, error){
+                    alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+                }
+    });
+
+    $("#createProductModal").modal('hide');
 }
 
-/*
-function doCreateStore(){
-s
+function openUpdateModal(e){
+    var clickedCard = $(e).closest("#myCard");
+    var storeIdData = $(clickedCard).children("div").children("#cardStoreId").text();
+    var productIdData = $(clickedCard).children("div").children("#cardProductId").text();
+    var nameData = $(clickedCard).children("div").children("h5").text();
+    var infoData = $(clickedCard).children("div").children("p").text();
+    var priceData = $(clickedCard).children("ul").children("li").children("span").text();
+
+    $("#updateStoreId").text(storeIdData);
+    $("#updateProductId").text(productIdData);
+    $('#updateProductNameForm').text(nameData);
+    $('#updateProductInfoForm').text(infoData);
+    $('#updateProductPriceForm').val(priceData);
+
+    $("#updateProductModal").modal('show');
+}
+
+function doUpdateProduct(productIdData){
     let sendData = {
-        name : ($('#storeNameForm').val()),
-        info : $('#storeInfoForm').val(),
-        tel : $('#storeTelForm').val(),
-        status : $("input[name=storeStatusRadio]:checked").val()
+        name : ($('#updateProductNameForm').val()),
+        info : $('#updateProductInfoForm').val(),
+        price : $('#updateProductPriceForm').val().replace(/\,/g,'')
     };
 
+    var storeId = $('#updateStoreId').text();
+    var productId = $('#updateProductId').text();
+
+    console.log(storeId);
+    console.log(productId);
     console.log(sendData);
 
     $.ajax({
-            type: "POST",
-            url: "/api/user/stores",
-            dataType: "JSON",
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(sendData),
-            success: function(data) {
-                console.log("저장 성공! uID is " + data);
-                location.href="/my-store";
-            },
-            error:function(request, status, error){
-                alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
-            }
+        type: "PUT",
+        url: "/api/user/stores/" + storeId +
+            "/products/" + productId,
+        dataType: "JSON",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(sendData),
+        success: function(data) {
+            console.log("수정 성공! uID is " + data);
+            location.href="/my-store/" + storeId + "/products";
+        },
+        error:function(request, status, error){
+            alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+        }
     });
 
-    $("#createStoreModal").modal('hide');
+    $("#updateProductModal").modal('hide');
 }
-
-*/
