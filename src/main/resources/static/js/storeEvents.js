da8436620030a9cc8fd022024edc641024ed8119
HoonDragonite <lsoonnoon@gmail.com>
2021-10-19 21:43:38 +0900

    Feat: 상점 이벤트 삭제 기능 구현

--- src/main/resources/static/js/storeEvents.js
+++ src/main/resources/static/js/storeEvents.js
@@ -27,13 +27,13 @@ function doCreateStoreEvents(){
 }
 
 function openUpdateModal(e){
-    var clickedCard = $(e).closest("#storeEventsRow");
-    var storeIdData = $(clickedCard).children("#tdStoreId").text();
-    var storeEventsIdData = $(clickedCard).children("#tdStoreEventsId").text();
-    var nameData = $(clickedCard).children("#tdStoreEventsName").text();
-    var infoData = $(clickedCard).children("#tdStoreEventsInfo").text();
-    var startDateData = $(clickedCard).children("#tdStoreEventsStartDate").text();
-    var endDateData = $(clickedCard).children("#tdStoreEventsEndDate").text();
+    var clickedRow = $(e).closest("#storeEventsRow");
+    var storeIdData = $(clickedRow).children("#tdStoreId").text();
+    var storeEventsIdData = $(clickedRow).children("#tdStoreEventsId").text();
+    var nameData = $(clickedRow).children("#tdStoreEventsName").text();
+    var infoData = $(clickedRow).children("#tdStoreEventsInfo").text();
+    var startDateData = $(clickedRow).children("#tdStoreEventsStartDate").text();
+    var endDateData = $(clickedRow).children("#tdStoreEventsEndDate").text();
 
     $("#updateStoreId").text(storeIdData);
     $("#updateStoreEventsId").text(storeEventsIdData);
@@ -74,4 +74,35 @@ function doUpdateStoreEvents(){
     });
 
     $("#updateStoreEventsModal").modal('hide');
+}
+
+function openDeleteModal(e){
+    var clickedRow = $(e).closest("#storeEventsRow");
+    var storeIdData = $(clickedRow).children("#tdStoreId").text();
+    var storeEventsIdData = $(clickedRow).children("#tdStoreEventsId").text();
+
+    $("#deleteStoreId").text(storeIdData);
+    $("#deleteStoreEventsId").text(storeEventsIdData);
+
+    $("#deleteStoreEventsModal").modal('show');
+}
+
+function doDeleteStoreEvents(){
+    var storeId = $('#deleteStoreId').text();
+    var storeEventsId = $('#deleteStoreEventsId').text();
+
+    $.ajax({
+        type: "DELETE",
+        url: "/api/user/stores/" + storeId +
+            "/store-events/" + storeEventsId,
+        contentType: 'application/json; charset=utf-8',
+        success: function() {
+            location.href="/my-store/" + storeId + "/store-events";
+        },
+        error:function(request, status, error){
+            alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+        }
+    });
+
+    $("#deleteStoreEventsModal").modal('hide');
 }
