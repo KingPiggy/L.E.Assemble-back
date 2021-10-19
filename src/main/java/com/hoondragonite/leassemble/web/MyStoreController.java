17527a1fd8cdf9fb793a935d6b782c03acdb47b9
HoonDragonite <lsoonnoon@gmail.com>
2021-10-19 18:45:19 +0900

    Feat: Store 이벤트 관리 페이지 작성

--- src/main/java/com/hoondragonite/leassemble/web/MyStoreController.java
+++ src/main/java/com/hoondragonite/leassemble/web/MyStoreController.java
@@ -3,8 +3,10 @@ package com.hoondragonite.leassemble.web;
 import com.hoondragonite.leassemble.config.auth.LoginUser;
 import com.hoondragonite.leassemble.config.auth.dto.SessionUser;
 import com.hoondragonite.leassemble.service.ProductService;
+import com.hoondragonite.leassemble.service.StoreEventsService;
 import com.hoondragonite.leassemble.service.StoreService;
 import com.hoondragonite.leassemble.web.dto.ProductResponseDto;
+import com.hoondragonite.leassemble.web.dto.StoreEventsResponseDto;
 import com.hoondragonite.leassemble.web.dto.StoreResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -19,8 +21,8 @@ import java.util.List;
 public class MyStoreController {
 
     private final StoreService storeService;
-
     private final ProductService productService;
+    private final StoreEventsService storeEventsService;
 
     @GetMapping("/my-store/{id}")
     public String myStoreInfo(Model model, @LoginUser SessionUser user, @PathVariable Long id) {
@@ -29,25 +31,41 @@ public class MyStoreController {
             model.addAttribute("loginUserImg", user.getPicture());
         }
 
-        StoreResponseDto store =  storeService.findById(id);
+        StoreResponseDto store = storeService.findById(id);
         model.addAttribute("store", store);
 
         return "my-store-info";
     }
 
     @GetMapping("/my-store/{id}/products")
-    public String myStoreProducts(Model model, @LoginUser SessionUser user, @PathVariable Long id){
+    public String myStoreProducts(Model model, @LoginUser SessionUser user, @PathVariable Long id) {
         if (user != null) {
             model.addAttribute("loginUserName", user.getName());
             model.addAttribute("loginUserImg", user.getPicture());
         }
 
-        StoreResponseDto store =  storeService.findById(id);
+        StoreResponseDto store = storeService.findById(id);
         model.addAttribute("store", store);
 
         List<ProductResponseDto> products = productService.findAllProductsByStoreId(id);
         model.addAttribute("products", products);
 
         return "my-store-products";
+    }
+
+    @GetMapping("/my-store/{id}/store-events")
+    public String myStoreEvents(Model model, @LoginUser SessionUser user, @PathVariable Long id) {
+        if (user != null) {
+            model.addAttribute("loginUserName", user.getName());
+            model.addAttribute("loginUserImg", user.getPicture());
+        }
+
+        StoreResponseDto store = storeService.findById(id);
+        model.addAttribute("store", store);
+
+        List<StoreEventsResponseDto> storeEvents = storeEventsService.findAllStoreEventsByStoreId(id);
+        model.addAttribute("storeEvents", storeEvents);
+
+        return "my-store-events";
     }
 }
